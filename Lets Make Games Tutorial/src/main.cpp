a62260c91f6aae26c6aa5b9d4daee8f1fa618054
KowalskiYukio <dzackwan@gmail.com>
2024-02-15 21:41:02 +0000

    Fixes window not launching but reverts SDL Image changes

--- Lets Make Games Tutorial/src/main.cpp
+++ Lets Make Games Tutorial/src/main.cpp
@@ -5,7 +5,7 @@ Game* game = nullptr;
 int main(int argc, char* argv[]) {
 	game = new Game();
 
-	game->init("C++ Let's Make Games Tutorial", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 2650, 1600, false);
+	game->init("C++ Let's Make Games Tutorial", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1920, 1080, false);
 
 	while (game->running()) {
 		game->handleEvents();
